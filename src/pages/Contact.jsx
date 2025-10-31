827ab641272eb352e012b49aaba463aea0272534
Ellis Lunayo <ellislunayo@gmail.com>
2025-10-31 22:27:18 +0300

    changed the location map widget

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,7 +19,7 @@ export default function Contact() {
             <CommentConatctFrom />
           </div>
           <div className="contact-map">
-            <GoogleMap addressLink="https://maps.app.goo.gl/5B5CXPrETK7uJKBX9" />
+            <GoogleMap addressLink="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31911.085591123723!2d36.780416!3d-1.23886!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f198ef85004c3%3A0xefb5fc6d518faa6!2sLa%20Villa%20Italian%20Restaurant%20%26%20Wine%20Bar!5e0!3m2!1sen!2sus!4v1761584314280!5m2!1sen!2sus" />
           </div>
         </div>
       </div>
