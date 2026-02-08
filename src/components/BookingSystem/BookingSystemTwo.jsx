60038cb3a4be605cd7b2f497c259b0e816741208
Ellis Lunayo <ellislunayo@gmail.com>
2026-02-08 09:32:48 +0300

    reseveration styling added

--- src/components/BookingSystem/BookingSystemTwo.jsx
+++ src/components/BookingSystem/BookingSystemTwo.jsx
@@ -12,6 +12,7 @@ export default function BookingSystemTwo({ videoId }) {
   useLayoutEffect(() => {
     imageZoomInOut(imageContainer.current, imageZoomIn.current);
   }, [videoId]);
+
   return (
     <>
       <div className="ak-height-150 ak-height-lg-60"></div>
@@ -27,7 +28,10 @@ export default function BookingSystemTwo({ videoId }) {
           <div className="row justify-content-center align-content-center align-items-center">
             <div className="col-md-6 col-12">
               <div className="ak-height-lg-60"></div>
-              <VideoButton videoId={videoId} />
+              {/* Added a relative z-index wrapper to ensure the button is clickable over the background */}
+              <div style={{ position: "relative", zIndex: "2" }}>
+                <VideoButton videoId={videoId} />
+              </div>
               <div className="ak-height-lg-60"></div>
             </div>
             <div className="col-md-6 col-12">
@@ -39,7 +43,8 @@ export default function BookingSystemTwo({ videoId }) {
                     animTwo={true}
                   />
                   <div className="ak-height-60 ak-height-lg-30"></div>
-                  <BookingSystem />
+                  {/* CRITICAL: Passing styleTwo prop here */}
+                  <BookingSystem styleTwo={true} />
                 </div>
               </div>
             </div>
