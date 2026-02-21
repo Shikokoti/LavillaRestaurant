a17c72a5bc663bd0034d56513179fb327b21ff1d
Ellis Lunayo <ellislunayo@gmail.com>
2026-02-21 11:54:22 +0300

    added the bookingsystem component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import FoodMenuHome from "../components/FoodMenu/FoodMenu";
 import BestItem from "../components/BestItem/BestItem";
 import Testimonial from "../components/Testimonial/Testimonial";
 import BookingSystemTwo from "../components/BookingSystem/BookingSystemTwo";
+import BookingSystem from "../components/BookingSystem/BookingSystem";
 import MovingText from "../components/MovingText/MovingText";
 import HeroSiderTwo from "../components/Silders/HeroSiderTwo";
 import PortfolioSlider from "../components/PortfolioSlider/PortfolioSlider";
