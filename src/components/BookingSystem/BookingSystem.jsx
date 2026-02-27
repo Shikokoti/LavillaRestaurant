795cc9f3b3acb05d376055682657af6415107d83
Ellis Lunayo <ellislunayo@gmail.com>
2026-02-27 17:20:25 +0300

    updated the reservation endpoint

--- src/components/BookingSystem/BookingSystem.jsx
+++ src/components/BookingSystem/BookingSystem.jsx
@@ -34,7 +34,7 @@ export default function BookingSystem(props) {
     };
 
     try {
-      const response = await fetch("http://api.lavilla.co.ke/create", {
+      const response = await fetch("https://api.lavilla.co.ke/create", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
